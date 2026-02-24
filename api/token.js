79c38596618a3c606776063f4d195845e55c0a1f
StreamVerse26 <aleksi.narvanen@hotmail.com>
2026-02-25 01:42:26 +0200

    Update token.js

--- api/token.js
+++ api/token.js
@@ -1,10 +1,4 @@
-let cachedToken = null;
-
 export default async function handler(req,res){
-
-if(cachedToken){
-return res.json(cachedToken);
-}
 
 const response = await fetch(
 "https://id.twitch.tv/oauth2/token",
@@ -13,15 +7,15 @@ method:"POST",
 headers:{
 "Content-Type":"application/x-www-form-urlencoded"
 },
-body:
-"client_id="+process.env.CLIENT_ID+
-"&client_secret="+process.env.CLIENT_SECRET+
-"&grant_type=client_credentials"
+body:new URLSearchParams({
+client_id: process.env.CLIENT_ID,
+client_secret: process.env.CLIENT_SECRET,
+grant_type: "client_credentials"
+})
 });
 
 const data = await response.json();
 
-cachedToken = data;
-
 res.json(data);
+
 }
