74b8ab961616d0a37e4fa7a2bac2fb128ce2e4bd
StreamVerse26 <aleksi.narvanen@hotmail.com>
2026-02-25 01:45:53 +0200

    Update token.js

--- api/token.js
+++ api/token.js
@@ -1,4 +1,11 @@
+let cachedToken = null;
+let expiresAt = 0;
+
 export default async function handler(req,res){
+
+if(cachedToken && Date.now() < expiresAt){
+return res.json(cachedToken);
+}
 
 const response = await fetch(
 "https://id.twitch.tv/oauth2/token",
@@ -10,11 +17,14 @@ headers:{
 body:new URLSearchParams({
 client_id: process.env.CLIENT_ID,
 client_secret: process.env.CLIENT_SECRET,
-grant_type: "client_credentials"
+grant_type:"client_credentials"
 })
 });
 
 const data = await response.json();
+
+cachedToken = data;
+expiresAt = Date.now() + (data.expires_in * 1000);
 
 res.json(data);
 
