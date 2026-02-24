771ed83c2f90b8691575f7cc691ece8de869ad99
StreamVerse26 <aleksi.narvanen@hotmail.com>
2026-02-25 00:59:11 +0200

    Update twitchUser.js

--- api/twitchUser.js
+++ api/twitchUser.js
@@ -2,16 +2,22 @@ export default async function handler(req,res){
 
 const { login, token } = req.query;
 
+if(!token){
+return res.status(400).json({error:"Missing token"});
+}
+
 const response = await fetch(
 "https://api.twitch.tv/helix/users?login="+login,
 {
 headers:{
 "Client-ID": process.env.CLIENT_ID,
-"Authorization":"Bearer "+token
+"Authorization": "Bearer " + token
 }
-});
+}
+);
 
 const data = await response.json();
+
 res.json(data);
 
 }
